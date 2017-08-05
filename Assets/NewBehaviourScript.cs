fa0e1965cc5a606b5e36a5296881864cf487205e
DavidUllauri <30758544+DavidUllauri@users.noreply.github.com>
2017-08-05 19:24:40 -0400

    New project

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("something changed");
+        print("something else changed");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
